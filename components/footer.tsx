fb6ec53e9d617a64a2e68eec8de08e3a447095ca
Huann <huannvictor@gmail.com>
2026-01-01 17:30:16 -0300

    feat(layout): add global footer component

---
+++ components/footer.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+	return (
+		<footer className="bg-muted p-7">
+			<p className="font-semibold text-foreground text-xs">
+				&copy; 2026 Aparatus
+			</p>
+			<p className="text-muted-foreground text-xs">
+				Todos os direitos reservados.
+			</p>
+		</footer>
+	)
+}
+
+export default Footer
